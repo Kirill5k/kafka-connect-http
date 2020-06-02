d1fda9ba5e0c88dfe9bdb7304536fdd236df1599
kirill5k <immotional@aol.com>
2020-06-02 20:51:56 +0100

    updated sink config

--- modules/sink/src/main/scala/io/kirill/kafka/connect/http/sink/HttpSinkConfig.scala
+++ modules/sink/src/main/scala/io/kirill/kafka/connect/http/sink/HttpSinkConfig.scala
@@ -1,45 +1,39 @@
 package io.kirill.kafka.connect.http.sink
 
-import org.apache.kafka.common.config.ConfigDef
+import java.util
+
+import org.apache.kafka.common.config.{AbstractConfig, ConfigDef}
 import org.apache.kafka.common.config.ConfigDef.{Importance, Type}
 import org.apache.kafka.connect.sink.SinkTask
 
+class HttpSinkConfig(
+    val props: util.Map[String, String]
+) extends AbstractConfig(HttpSinkConfig.config, props) {
+  val httpApiUrl        = getString(HttpSinkConfig.HTTP_API_URL)
+  val httpRequestMethod = getString(HttpSinkConfig.HTTP_REQUEST_METHOD)
+  val batchSize         = getInt(HttpSinkConfig.BATCH_SIZE)
+  val maxRetries        = getInt(HttpSinkConfig.MAX_RETRIES)
+}
+
 object HttpSinkConfig {
   val HTTP_API_URL     = "http.api.url"
-  val HTTP_API_URL_DOC = "HTTP API URL"
+  val HTTP_API_URL_DOC = "Http api url where the data will be sent"
 
   val HTTP_REQUEST_METHOD         = "http.request.method"
   val HTTP_REQUEST_METHOD_DOC     = "HTTP Request Method"
   val HTTP_REQUEST_METHOD_DEFAULT = "POST"
 
-  val MAX_RETRIES         = "max.retries"
-  val MAX_RETRIES_DEFAULT = 10
-  val MAX_RETRIES_DOC     = "The maximum number of times to retry on errors before failing the task"
+  val BATCH_SIZE         = "batch.size"
+  val BATCH_SIZE_DOC     = "The number of records accumulated in a batch before the HTTP API will be invoked"
+  val BATCH_SIZE_DEFAULT = 1
 
-  val TOPICS     = SinkTask.TOPICS_CONFIG
-  val TOPICS_DOC = "The Kafka topic to read from."
+  val MAX_RETRIES         = "max.retries"
+  val MAX_RETRIES_DOC     = "The maximum number of times to retry on errors before failing the task"
+  val MAX_RETRIES_DEFAULT = 10
 
   val config: ConfigDef = new ConfigDef()
-    .define(
-      HTTP_API_URL,
-      ConfigDef.Type.STRING,
-      ConfigDef.NO_DEFAULT_VALUE,
-      ConfigDef.Importance.HIGH,
-      HTTP_API_URL_DOC
-    )
-    .define(
-      HTTP_REQUEST_METHOD,
-      ConfigDef.Type.STRING,
-      HTTP_REQUEST_METHOD_DEFAULT,
-      ConfigDef.Importance.HIGH,
-      HTTP_REQUEST_METHOD_DOC
-    )
-    .define(
-      MAX_RETRIES,
-      ConfigDef.Type.INT,
-      MAX_RETRIES_DEFAULT,
-      ConfigDef.Importance.MEDIUM,
-      MAX_RETRIES_DOC
-    )
-    .define(TOPICS, Type.LIST, Importance.HIGH, TOPICS_DOC)
+    .define(HTTP_API_URL, Type.STRING, ConfigDef.NO_DEFAULT_VALUE, Importance.HIGH, HTTP_API_URL_DOC)
+    .define(HTTP_REQUEST_METHOD, Type.STRING, HTTP_REQUEST_METHOD_DEFAULT, Importance.HIGH, HTTP_REQUEST_METHOD_DOC)
+    .define(MAX_RETRIES, Type.INT, MAX_RETRIES_DEFAULT, Importance.MEDIUM, MAX_RETRIES_DOC)
+    .define(BATCH_SIZE, Type.INT, BATCH_SIZE_DEFAULT, Importance.MEDIUM, BATCH_SIZE_DOC)
 }
