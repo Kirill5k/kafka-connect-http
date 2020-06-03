8e839707367a7c854a831065512712ce4edbf838
kirill5k <immotional@aol.com>
2020-06-03 10:33:30 +0100

    avro converter

--- build.sbt
+++ build.sbt
@@ -2,6 +2,13 @@ ThisBuild / scalaVersion     := "2.13.2"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "io.kirill"
 ThisBuild / organizationName := "example"
+ThisBuild / resolvers ++= Seq(
+  "Confluent" at "https://packages.confluent.io/maven/",
+  Resolver.mavenLocal,
+  Resolver.sonatypeRepo("public"),
+  Resolver.sbtPluginRepo("releases")
+)
+
 
 lazy val root = (project in file("."))
   .settings(
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,22 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    lazy val connectApi = "2.5.0"
+    lazy val kafka     = "2.5.0"
+    lazy val confluent = "5.5.0"
 
     lazy val scalatest = "3.1.1"
   }
 
   object Libraries {
-    lazy val connectApi = "org.apache.kafka" % "connect-api" % Versions.connectApi
+    lazy val connectApi           = "org.apache.kafka" % "connect-api"                  % Versions.kafka
+    lazy val connectAvroConverter = "io.confluent"     % "kafka-connect-avro-converter" % Versions.confluent
 
     lazy val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalatest
   }
 
   lazy val sink = Seq(
-    Libraries.connectApi
+    Libraries.connectApi,
+    Libraries.connectAvroConverter
   )
 
   lazy val test = Seq(
