2b84c07e59fe61dda5ee92524a0032e5e95b4bb1
kirill5k <immotional@aol.com>
2020-06-06 09:18:28 +0100

    cleaned dockerfiles

--- modules/sink/docker/run.sh
+++ modules/sink/docker/run.sh
@@ -4,9 +4,7 @@ while ! nc -z kafka 9092; do
   sleep 3
 done
 
-echo "Kafka is now listening. Starting Connect"
-
-sleep 10
+echo "kafka is now listening. Starting Connect"
 
 export CLASSPATH="/usr/share/java/*"
 /opt/kafka/bin/connect-distributed.sh /opt/kafka/config/connect-distributed.properties
